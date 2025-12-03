7a061b0101dcb75440d04dc536b056bbca58f681
zneedell <zneedell@gmail.com>
2025-12-03 15:48:36 -0800

    env fix

--- src/consist/models/run.py
+++ src/consist/models/run.py
@@ -1,7 +1,8 @@
 import uuid
 from typing import Dict, Any, List, Optional
 from datetime import datetime
-from datetime import UTC
+from datetime import timezone
+UTC = timezone.utc
 from sqlalchemy import Column, JSON
 from sqlmodel import Field, SQLModel
 from consist.models.artifact import Artifact
